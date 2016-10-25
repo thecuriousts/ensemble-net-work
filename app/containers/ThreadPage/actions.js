7539f6a55a36e24b8b5541585dce8e953b06b6d1
Peram Sathyam <sathyam.peram@gmail.com>
2016-10-25 19:36:03 +0200

    Add initial boilerplate code

---
+++ app/containers/ThreadPage/actions.js
@@ -0,0 +1,24 @@
+/*
+ *
+ * ThreadPage actions
+ *
+ */
+
+import {
+  SEND_MESSAGE,
+  CHANGE_MESSAGE
+} from './constants';
+
+export function changeMessage(message) {
+  return {
+    type: CHANGE_MESSAGE,
+    message
+  };
+}
+
+export function sendMessage(message) {
+  return {
+    type: SEND_MESSAGE,
+    message
+  };
+}
---
+++ app/containers/ThreadPage/constants.js
@@ -0,0 +1,8 @@
+/*
+ *
+ * ThreadPage constants
+ *
+ */
+
+export const SEND_MESSAGE = 'optimistic/ThreadPage/SEND_MESSAGE';
+export const CHANGE_MESSAGE = 'optimistic/ThreadPage/CHANGE_MESSAGE';
